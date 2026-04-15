5fffbdc1bfcde3129dd60ab6b648cbdd4ddeb0fe
William Josue Bermudez Flores <wiliangamer838@gmail.com>
2026-04-15 10:32:37 -0600

    Agregar archivos de proyecto.

---
+++ Parcial 2/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Parcial_2
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textmarca = new System.Windows.Forms.TextBox();
+            this.textmodelo = new System.Windows.Forms.TextBox();
+            this.textprecio = new System.Windows.Forms.TextBox();
+            this.btagregar = new System.Windows.Forms.Button();
+            this.dttodo = new System.Windows.Forms.DataGridView();
+            this.Marca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dttodo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Marca";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(258, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Modelo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(459, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Precio";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textmarca
+            // 
+            this.textmarca.Location = new System.Drawing.Point(41, 67);
+            this.textmarca.Name = "textmarca";
+            this.textmarca.Size = new System.Drawing.Size(132, 22);
+            this.textmarca.TabIndex = 3;
+            this.textmarca.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textmodelo
+            // 
+            this.textmodelo.Location = new System.Drawing.Point(261, 67);
+            this.textmodelo.Name = "textmodelo";
+            this.textmodelo.Size = new System.Drawing.Size(127, 22);
+            this.textmodelo.TabIndex = 4;
+            // 
+            // textprecio
+            // 
+            this.textprecio.Location = new System.Drawing.Point(464, 67);
+            this.textprecio.Name = "textprecio";
+            this.textprecio.Size = new System.Drawing.Size(142, 22);
+            this.textprecio.TabIndex = 5;
+            // 
+            // btagregar
+            // 
+            this.btagregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btagregar.Location = new System.Drawing.Point(261, 143);
+            this.btagregar.Name = "btagregar";
+            this.btagregar.Size = new System.Drawing.Size(129, 36);
+            this.btagregar.TabIndex = 6;
+            this.btagregar.Text = "Agregar";
+            this.btagregar.UseVisualStyleBackColor = true;
+            this.btagregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dttodo
+            // 
+            this.dttodo.AllowUserToDeleteRows = false;
+            this.dttodo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dttodo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Marca,
+            this.Modelo,
+            this.Precio});
+            this.dttodo.Location = new System.Drawing.Point(25, 213);
+            this.dttodo.Name = "dttodo";
+            this.dttodo.ReadOnly = true;
+            this.dttodo.RowHeadersWidth = 51;
+            this.dttodo.RowTemplate.Height = 24;
+            this.dttodo.Size = new System.Drawing.Size(616, 246);
+            this.dttodo.TabIndex = 7;
+            this.dttodo.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Marca
+            // 
+            this.Marca.HeaderText = "Marca";
+            this.Marca.MinimumWidth = 6;
+            this.Marca.Name = "Marca";
+            this.Marca.ReadOnly = true;
+            this.Marca.Width = 125;
+            // 
+            // Modelo
+            // 
+            this.Modelo.HeaderText = "Modelo";
+            this.Modelo.MinimumWidth = 6;
+            this.Modelo.Name = "Modelo";
+            this.Modelo.ReadOnly = true;
+            this.Modelo.Width = 125;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.MinimumWidth = 6;
+            this.Precio.Name = "Precio";
+            this.Precio.ReadOnly = true;
+            this.Precio.Width = 125;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 497);
+            this.Controls.Add(this.dttodo);
+            this.Controls.Add(this.btagregar);
+            this.Controls.Add(this.textprecio);
+            this.Controls.Add(this.textmodelo);
+            this.Controls.Add(this.textmarca);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "form1";
+            this.Text = "Sistema De Gestión De Vehiculos";
+            this.Load += new System.EventHandler(this.form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dttodo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textmarca;
+        private System.Windows.Forms.TextBox textmodelo;
+        private System.Windows.Forms.TextBox textprecio;
+        private System.Windows.Forms.Button btagregar;
+        private System.Windows.Forms.DataGridView dttodo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Marca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Modelo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+    }
+}
+
---
+++ Parcial 2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial_2
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+
+
+        }
+        public class vehiculo
+        {
+            public string marca { get; set; }
+            public string modelo { get; set; }
+            public double precio { get; set; }
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            {
+                DataGridViewRow fila = dttodo.Rows[e.RowIndex];
+
+                string marca = fila.Cells[0].Value.ToString();
+                string modelo = fila.Cells[1].Value.ToString();
+                string precio = fila.Cells[2].Value.ToString();
+
+                MessageBox.Show(
+                    "Resumen del vehículo:\n\n" +
+                    "Marca: " + marca + "\n" +
+                    "Modelo: " + modelo + "\n" +
+                    "Precio: $" + precio,
+                    "Información",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+                }
+
+
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textmarca.Text) && !string.IsNullOrEmpty(textmodelo.Text) &&  !string.IsNullOrEmpty(textprecio.Text))
+            {
+                double precio = Convert.ToDouble(textprecio.Text);
+               
+                dttodo.Rows.Add(textmarca.Text, textmodelo.Text, textprecio.Text);
+                textmodelo.Clear();
+                textprecio.Clear();
+                textmarca.Clear();
+               
+            }
+            else
+            {
+                MessageBox.Show("Por favor complete los campos.");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
